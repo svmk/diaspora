c7e155b8348d719c3df6bf098db94d41b5765400
maxwell <maxwell@joindiaspora.com>
2011-03-16 23:44:41 -0700

    move processing to photo callback

--- app/controllers/status_messages_controller.rb
+++ app/controllers/status_messages_controller.rb
@@ -56,7 +56,6 @@ class StatusMessagesController < ApplicationController
           end
         end
         photos.update_all(:pending => false, :public => public_flag)
-        photos.each{|x| x.queue_processing_job; puts "i did it"}
       end
 
       respond_to do |format|
